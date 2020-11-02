924a2bc272e1ccb7f3244de12a4d9d3d14a1ce92
Michael Gann <michael.gann@live.com>
2020-11-02 15:37:16 -0700

    create user sign-up route and form

--- db/migrations/20201102183454-create-user.js
+++ db/migrations/20201102183454-create-user.js
@@ -1,55 +1,54 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    return queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       username: {
         allowNull: false,
         type: Sequelize.STRING,
-        unique: true
+        unique: true,
       },
       email: {
         allowNull: false,
         type: Sequelize.STRING,
-        unique: true
+        unique: true,
       },
       hashedPassword: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       bookcryptsId: {
-        allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'Crypts' }
+        references: { model: "Crypts" },
       },
       birthdate: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       fullName: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       gender: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+    return queryInterface.dropTable("Users");
+  },
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require("./utils");
+const { check, validationResult } = require("express-validator");
+const db = require("../db/models");
+const bcrypt = require("bcryptjs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
+router.get("/sign-up", csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render("sign-up-form", {
+    title: "New User",
+    user,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+router.post(
+  "/sign-up",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const { username, email, birthdate, gender, fullName, password } = req.body;
+    console.log(username, email, birthdate);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await db.User.create({
+      username,
+      email,
+      birthdate,
+      gender,
+      fullName,
+      hashedPassword,
+    });
+
+    try {
+      // await user.save();
+      console.log("hi");
+      res.redirect("/");
+    } catch (e) {
+      //do something with the error
+    }
+  })
+);
+
+router.post("/");
 module.exports = router;
