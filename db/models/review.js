1d3014275a5a3be34b2da1682222faedef38327b
Michael Gann <michael.gann@live.com>
2020-11-05 13:27:55 -0700

    books display and reviews display also footer

--- db/models/review.js
+++ db/models/review.js
@@ -1,16 +1,24 @@
-'use strict';
+"use strict";
+
+const User = require("./user");
+
 module.exports = (sequelize, DataTypes) => {
-  const Review = sequelize.define('Review', {
-    review: DataTypes.TEXT,
-    userId: DataTypes.INTEGER
-  }, {});
-  Review.associate = function(models) {
+  const Review = sequelize.define(
+    "Review",
+    {
+      review: DataTypes.TEXT,
+      userId: DataTypes.INTEGER,
+    },
+    {}
+  );
+  Review.associate = function (models) {
     const columnMapping = {
-      through: 'ReviewJoinBook',
-      otherKey: 'bookId',
-      foreignKey: 'reviewId'
-    }
+      through: "ReviewJoinBook",
+      otherKey: "bookId",
+      foreignKey: "reviewId",
+    };
     Review.belongsToMany(models.Book, columnMapping);
+    Review.belongsTo(models.User, { foreignKey: "userId" });
   };
   return Review;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { asyncHandler } = require("./utils");
+const { User, Review, ReviewJoinBook, Book } = require("../db/models");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "deadreads" });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const reviews = await Review.findAll({
+      include: {
+        model: User,
+        required: true,
+      },
+    });
+
+    const books = await Book.findAll({ limit: 5 });
+
+    res.render("index", { title: "deadreads", reviews, books });
+  })
+);
 
 module.exports = router;
