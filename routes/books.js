a0b8e2704892bc487c13626d45a97c44cdbbd199
= <davidleegriffin@yahoo.com>
2020-11-05 13:10:42 -0600

    add books/:id route and styled books pages

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { asyncHandler } = require('./utils');
-const { Book } = require('../db/models');
+
+const { requireAuth } = require("../auth");
+const { asyncHandler } = require("./utils");
+const { Crypt, User, Book, CryptJoinBook } = require("../db/models");
 
 router.get('/', asyncHandler(async (req, res) => {
     const books = await Book.findAll();
     res.render('books', { title: 'Books', books });
 }));
+
+router.get(
+            '/:id(\\d+)',
+            requireAuth,
+            asyncHandler(async(req, res) => {
+                const bookId = parseInt(req.params.id);
+                const book = await Book.findByPk(bookId);
+                res.render('book', { title: `${book.title}`, book });
+}))
 
 
 
